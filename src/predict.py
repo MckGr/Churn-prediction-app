ea81c1f010e49f4a3709e054c4bf17153b4bd503
MckGr <163030272+MckGr@users.noreply.github.com>
2026-02-26 13:43:36 +0100

    predict updated

--- src/predict.py
+++ src/predict.py
@@ -1,14 +1,15 @@
 import joblib
 import pandas as pd
 
-model = joblib.load('models/churn_model.pkl')
+model = joblib.load("models/churn_model.pkl")
 
-def predict(data: dict) -> dict:
 
+def predict_customer(data: dict) -> dict:
     df = pd.DataFrame([data])
-    
+
     proba = model.predict_proba(df)[0][1]
 
-    return {"churn_probability": round(float(proba), 4),
-            "prediction": int(proba > 0.5)
-            }
+    return {
+        "churn_probability": round(float(proba), 4),
+        "prediction": int(proba > 0.5)
+    }
